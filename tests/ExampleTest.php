ddb1978783b2d41d992561d5c6e28a9532b7bc71
Kelt Dockins <kelt@dockins.org>
2014-02-12 23:00:14 -0600

    first commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php namespace Definitely246\Support;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 */
+	public function testBasicExample()
+	{
+
+	}
+}
---
+++ tests/View/PhpEngineTest.php
@@ -0,0 +1,16 @@
+<?php namespace Definitely246\Support\View;
+
+class PhpEngineTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 */
+	public function testBasicExample()
+	{
+		new PhpEngine;
+	}
+
+	// need to test PhpEngine ->get function some later
+}
